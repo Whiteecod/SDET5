fbd48b1ddb6db5a4a2cf27a5fd7e53ed7dfc74f2
Kubil <kubilayculhaaa@gmail.com>
2023-02-01 17:40:43 +0300

    Initial commit

---
+++ src/Gun01/MainApp.java
@@ -0,0 +1,15 @@
+package Gun01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainApp {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.techno.study/");
+
+    }
+}
